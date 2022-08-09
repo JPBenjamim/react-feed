b987b7ddc6c5c64507395cf880f3af7e4ff92271
Jpbenjamim <dev.bejamim@gmail.com>
2022-08-09 10:44:31 -0300

    feat(Post):creating post props

--- react-feed/src/App.jsx
+++ react-feed/src/App.jsx
@@ -1,3 +1,17 @@
+import { Post } from './Post'
+
 export function App() {
-  return <h1>react feed</h1>
+  return (
+    <div>
+      <Post
+        author="Jonas Benjamim"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi expedita tempora doloremque inventore quos vel libero ut dolor, explicabo ab, sunt"
+      />
+      <Post
+        author="Gabriel linn"
+        content="Um novo post legal"
+      />
+      
+    </div>
+  )
 }
