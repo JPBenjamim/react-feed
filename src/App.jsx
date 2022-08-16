daccdf2e704ae207742dcabd9d868af1fe1c5392
Jpbenjamim <dev.bejamim@gmail.com>
2022-08-16 17:23:46 -0300

    feat(header)creating Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import styles from './App.module.css'
+
 import { Header } from './components/Header'
 import { Post } from './Post'
 
 import './global.css'
+import { Sidebar } from './components/Siderbar'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post
         author="Jonas Benjamim"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi expedita tempora doloremque inventore quos vel libero ut dolor, explicabo ab, sunt"
       />
@@ -15,6 +22,9 @@ export function App() {
         author="Gabriel linn"
         content="Um novo post legal"
       />
+        </main>
+
+      </div>
       
     </div>
   )
