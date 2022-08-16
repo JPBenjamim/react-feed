d7ca1a78ceb70840db79d00feae8e734c99c86ab
Jpbenjamim <dev.bejamim@gmail.com>
2022-08-16 16:32:15 -0300

    feat(Header):creating css header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Jonas Benjamim"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi expedita tempora doloremque inventore quos vel libero ut dolor, explicabo ab, sunt"
