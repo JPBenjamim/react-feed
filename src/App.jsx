91dacc6582fbfaa04d5e9384cc4cbc9ffbcfff4a
Jpbenjamim <dev.bejamim@gmail.com>
2022-08-18 17:48:57 -0300

    feat(Header):add component post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from './App.module.css'
 
 import { Header } from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Siderbar'
 
 import './global.css'
-import { Sidebar } from './components/Siderbar'
+
 
 export function App() {
   return (
